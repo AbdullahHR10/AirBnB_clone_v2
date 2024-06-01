88561704bb326477a0bd61af9f917a08a1fff729
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-06-01 01:24:54 -0700

    Write the script

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+# Sets up your servers for the deployment of web_static
+
+# Installs Nginx if it not already installed
+apt update
+nginx -v
+apt install nginx -y
+
+# Creates the folder /data/ if it doesn’t already exist
+mkdir -p /data/
+
+# Creates the folder /data/web_static/ if it doesn’t already exist
+mkdir -p /data/web_static/
+
+# Creates the folder /data/web_static/releases/ if it doesn’t already exist
+mkdir -p /data/web_static/releases/
+
+# Creates the folder /data/web_static/shared/ if it doesn’t already exist
+mkdir -p /data/web_static/shared/
+
+# Creates the folder /data/web_static/releases/test/ if it doesn’t already exist
+mkdir -p ~/data/web_static/releases/test/
+
+# Creates a fake HTML file /data/web_static/releases/test/index.html to test Nginx configuration
+echo '<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>' > /data/web_static/releases/test/index.html
+
+# Creates a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/ folder. If the symbolic link already exists, it should be deleted and recreated every time the script is ran.
+ln -s -f /data/web_static/releases/test/ /data/web_static/current
+
+# Gives ownership of the /data/ folder to the ubuntu user AND group (you can assume this user and group exist). This should be recursive; everything inside should be created/owned by this user/group.
+chown -R ubuntu:ubuntu /data/
+
+# Updates the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static (ex: https://mydomainname.tech/hbnb_static). Don’t forget to restart Nginx after updating the configuration:
+# ...Uses alias inside Nginx configuration
+sudo sed -i 's@^\s*location \/hbnb_static\/.*$@  location /hbnb_static/ {\n    alias /data/web_static/current/;\n  }\n@g' /etc/nginx/sites-available/default
+service nginx restart
