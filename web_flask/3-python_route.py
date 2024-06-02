d1bce40c10ff3268f5380b9831ed2daf11488d27
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-06-02 03:15:57 -0700

    Write the script

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""A simple Flask web application with 4 routes"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Route that returns 'Hello HBNB!'"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Route that returns 'HBNB'"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """Route that returns 'C ' followed by the value of the text variable.
+    Replaces underscores with spaces."""
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    """Reotue that returns 'Python ' followed by the value of the text variable.
+    Replaces underscores with spaces."""
+    return 'Python ' + text.replace('_', ' ')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
