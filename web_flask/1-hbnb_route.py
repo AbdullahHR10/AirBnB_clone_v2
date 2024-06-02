89ebf022c4dd1f45a736d6d6d7fe1ea6ef09f4c6
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-06-02 02:55:51 -0700

    Fix pycodestyle

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -11,6 +11,7 @@ def hello_hbnb():
     """Route that returns 'Hello HBNB!'"""
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """Route that returns 'HBNB'"""
