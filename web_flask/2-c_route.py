c7efb37b1b085a1f1742dd6cbcfe77545678de3c
AbdullahHR10 <AbdullahH.Ragab10>
2024-07-25 20:26:00 -0700

    Add script that starts a Flask web application

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module that starts a Flask web application."""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def Hello_HBNB():
+    """Displays 'Hello HBNB!' for the root url"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    """Displays 'HBNB' for the 'hbnb' url"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def C_is_fun(text):
+    """Displays “C ” followed by the value of the text variable"""
+    return f"C {text.replace('_', ' ')}"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
