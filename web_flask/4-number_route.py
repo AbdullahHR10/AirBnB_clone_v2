508a304ba1f25a8c76c67dde0ceff8506d12a5b7
AbdullahHR10 <AbdullahH.Ragab10>
2024-07-25 23:23:17 -0700

    Add script that starts a Flask web application

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Module that starts a Flask web application."""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def Hello_HBNB():
+    """Displays 'Hello HBNB!' for the root url"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    """Displays 'HBNB' for the 'hbnb' url"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def C_is_fun(text):
+    """Displays “C ”, followed by the value of the text variable"""
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def Python_is_cool(text='is_cool'):
+    """Displays “Python ”, followed by the value of the text variable"""
+    return f"Python {text.replace('_', ' ')}"
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def is_it_number(n):
+    """Displays “n is a number” only if n is an integer"""
+    n = str(n)
+    return f"{n} is a number"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
