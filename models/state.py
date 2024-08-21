bf0e276fb1f2153313e6f3de8fcc45f7f6217d3d
AbdullahHR10 <AbdullahH.Ragab10>
2024-08-21 09:50:23 -0700

    Fix some errors

--- models/state.py
+++ models/state.py
@@ -1,31 +1,25 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-import os
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.base_model import BaseModel, Base
 from models.city import City
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
-    name = Column(
-        String(128), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship(
-            'City',
-            cascade='all, delete, delete-orphan',
-            backref='state'
-        )
-    else:
-        @property
-        def cities(self):
-            """Returns the cities in this State"""
-            from models import storage
-            cities_in_state = []
-            for value in storage.all(City).values():
-                if value.state_id == self.id:
-                    cities_in_state.append(value)
-            return cities_in_state
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref='state', cascade='all, delete-orphan')
+
+    @property
+    def cities_list(self):
+        """Returns the list of City instances with state_id equals to
+        the current State.id"""
+        from models import storage
+        all_cities = storage.all(City)
+        city_list = []
+        for city in all_cities.values():
+            if city.state_id == self.id:
+                city_list.append(city)
+        return city_list
